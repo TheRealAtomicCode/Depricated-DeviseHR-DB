2b7cdd0684d9105c7df046c9b34308a8be21d9a6
Abd al Qader Baghi <aqbaghi@atomiccode.uk>
2024-02-06 00:13:04 +0200

    added an Id field to the Hierarchies table and make the companyId none nullable

--- Database/Models/Hierarchies.sql
+++ Database/Models/Hierarchies.sql
@@ -1,8 +1,9 @@
 CREATE TABLE Hierarchies (
+    id SERIAL PRIMARY KEY,
     manager_id INTEGER,
     subordinate_id INTEGER,
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES Users (id),
     CONSTRAINT fk_subordinate FOREIGN KEY (subordinate_id) REFERENCES Users (id),
-    CONSTRAINT pk_hierarchies PRIMARY KEY (manager_id, subordinate_id),
+    CONSTRAINT uq_hierarchies UNIQUE (manager_id, subordinate_id),
     CHECK (manager_id <> subordinate_id)
 );
