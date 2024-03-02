423891a6ee347b6fa7fee9dd7cff7adc9ac0ecbc
Abd al Qader Baghi <aqbaghi@atomiccode.uk>
2024-03-02 23:27:26 +0200

    changed the cascading rules on the Hierarchy table so that the relationship between manager and subordinate gets deleted on change for the roles

--- Database/Models/Hierarchies.sql
+++ Database/Models/Hierarchies.sql
@@ -1,9 +1,24 @@
 CREATE TABLE Hierarchies (
     id SERIAL PRIMARY KEY,
-    manager_id INTEGER,
-    subordinate_id INTEGER,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES Users (id),
-    CONSTRAINT fk_subordinate FOREIGN KEY (subordinate_id) REFERENCES Users (id),
+    manager_id INTEGER NOT NULL,
+    subordinate_id INTEGER NOT NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES Users (id) ON DELETE CASCADE,
+    CONSTRAINT fk_subordinate FOREIGN KEY (subordinate_id) REFERENCES Users (id) ON DELETE CASCADE,
     CONSTRAINT uq_hierarchies UNIQUE (manager_id, subordinate_id),
     CHECK (manager_id <> subordinate_id)
 );
+
+
+
+-- changed the cascade rules so that the hierarchy can get deleted when the role changes
+
+-- CREATE TABLE Hierarchies (
+--     id SERIAL PRIMARY KEY,
+--     manager_id INTEGER NOT NULL,
+--     subordinate_id INTEGER NOT NULL,
+--     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES Users (id),
+--     CONSTRAINT fk_subordinate FOREIGN KEY (subordinate_id) REFERENCES Users (id),
+--     CONSTRAINT uq_hierarchies UNIQUE (manager_id, subordinate_id),
+--     CHECK (manager_id <> subordinate_id)
+-- );
+
