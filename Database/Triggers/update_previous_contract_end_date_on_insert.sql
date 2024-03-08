d0c240cc18cd6d721579396d3581ecd68c7c3e13
Abd al Qader Baghi <aqbaghi@atomiccode.uk>
2024-03-09 00:19:12 +0200

    added the new leave year when inserting the conract however the leave year needs to be checked if it exists within the current year to add a contract and also needs to have the current year of the contract when inserted and not 1970

--- Database/Triggers/update_previous_contract_end_date_on_insert.sql
+++ Database/Triggers/update_previous_contract_end_date_on_insert.sql
@@ -1,3 +1,6 @@
+
+
+
 CREATE OR REPLACE FUNCTION update_previous_contract_end_date_on_insert()
 RETURNS TRIGGER AS $$
 DECLARE
@@ -5,6 +8,8 @@ DECLARE
     new_start_day INT;
     last_end_day INT;
     new_end_date DATE;
+ 	saved_contracted_start_date TIMESTAMP;
+	saved_company_id INT;
 BEGIN
     SELECT *
     INTO last_contract
@@ -46,10 +51,33 @@ BEGIN
             RETURN NEW;
         END IF;
     ELSE
-        -- if no previous contract, add contract
+		RAISE NOTICE 'made it here';
+		-- get leave start date from user
+		SELECT contracted_leave_start_date, company_id INTO saved_contracted_start_date, saved_company_id FROM users WHERE id = NEW.user_id;
+		
+		RAISE NOTICE '%', saved_company_id;
+		RAISE NOTICE '%', saved_contracted_start_date;
+		
+		IF saved_contracted_start_date IS NULL THEN
+			RAISE NOTICE 'made it here 2';
+			-- get leave start date from company if the user does not have a leave year start date
+    		SELECT annual_leave_start_date INTO saved_contracted_start_date FROM companies WHERE id = saved_company_id;
+			
+			RAISE NOTICE '%', saved_contracted_start_date;
+  		END IF;
+		
+		RAISE NOTICE 'made it here 3';
+        -- if no previous contract, then new contract will be added by insert
+		INSERT INTO leave_year (user_id, company_id, leave_year_start_date, total_leave_entitlement, total_leave_allowance, added_by)
+		VALUES (NEW.user_id, NEW.company_id, saved_contracted_start_date, NEW.contracted_leave_entitlement, NEW.this_contracts_leave_allowence, NEW.user_id);
+		
+		RAISE NOTICE 'made it here 4';
         RETURN NEW;
+		
     END IF;
 
+	-- this code will not run if first contract
+	-- update previous contract to end of start of new contract
     UPDATE Contracts
     SET end_date = NEW.start_date
     WHERE id = last_contract.id;
